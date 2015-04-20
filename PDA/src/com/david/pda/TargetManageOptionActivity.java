55b24c69944f4b68540a8a6f1ae2f0d4043012e8
guimaodai <wyyxdgm@163.com>
2015-04-20 15:12:22 +0800

    add popupwindow for target manage option,except the function of add or update target

--- PDA/src/com/david/pda/TargetManageOptionActivity.java
+++ PDA/src/com/david/pda/TargetManageOptionActivity.java
@@ -4,24 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.RectF;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.david.pda.sqlite.model.Target;
+import com.david.pda.sqlite.model.util.DemoDB;
 import com.david.pda.util.other.Bind;
 import com.david.pda.util.other.DrawUtil;
 import com.david.pda.weather.model.util.L;
@@ -31,6 +41,7 @@ public class TargetManageOptionActivity extends Activity {
 	ImageButton backward;
 	GridView gridView;
 	LinearLayout linearLayout;
+	RelativeLayout topBar;
 	Canvas c;
 	Paint p;
 	RectF rectf = new RectF(0, 0, 400, 400);// 380/2=190
@@ -38,14 +49,17 @@ public class TargetManageOptionActivity extends Activity {
 	private List<Target> targets = new ArrayList<Target>();
 	private Target startTarget;
 	private Target endTarget;
+	private long[] times = new long[] { 1l, 2l };
+	private PopupWindow popupWindow;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.target_manage_option);
+		setContentView(R.layout.main_target_manage_option);
 		backward = (ImageButton) findViewById(R.id.main_target_manage_option_topbar_backward);
 		imageView = (ImageView) findViewById(R.id.main_target_manage_option_imageView);
 		gridView = (GridView) findViewById(R.id.main_target_manage_option_gridView);
+		topBar = (RelativeLayout) findViewById(R.id.main_target_manage_option_topbar);
 		linearLayout = (LinearLayout) findViewById(R.id.main_target_manage_option_linearLayout);
 		Bind.bindReturn(backward, TargetManageOptionActivity.this,
 				MainActivity.class, MainActivity.POSTION_TARGET_MANAGE);
@@ -62,14 +76,6 @@ public class TargetManageOptionActivity extends Activity {
 						+ ","
 						+ (imageView.getScaleY() + imageView.getHeight() / 2)
 						+ "]");
-		// Display display = getWindowManager().getDefaultDisplay(); //
-		// Activity#getWindowManager()
-		// Point size = new Point();
-		// display.getSize(size);
-		// Log.i(L.t, "width:" + size.x);
-		// Log.i(L.t, "height:" + size.y);
-		// Log.i(L.t, "center----:[" + size.x / 2 + "," + size.y / 2 + "]");
-		// Log.i(L.t, "leftEdge:" + (size.x / 2 - 190));
 		loadData();
 		drawDataToBitmap();// 利用数据 draw bitmap
 		imageView.setImageBitmap(currentBitmap);
@@ -89,6 +95,7 @@ public class TargetManageOptionActivity extends Activity {
 			case MotionEvent.ACTION_DOWN: {// 按下时触发
 				Log.i(L.mo, "ACTION_DOWN" + text);
 				startTarget = getBlockIndexByXY(e.getX(), e.getY());
+				times[0] = System.currentTimeMillis();
 				if (startTarget != null)
 					Log.i(L.mo, "startTarget:" + startTarget.getName());
 			}
@@ -100,9 +107,9 @@ public class TargetManageOptionActivity extends Activity {
 				// 触摸后触发
 				Log.i(L.mo, "ACTION_UP" + text);
 				endTarget = getBlockIndexByXY(e.getX(), e.getY());
+				times[1] = System.currentTimeMillis();
 				if (endTarget != null)
 					Log.i(L.mo, "endTarget:" + endTarget.getName());
-
 				doAfterActionUp();
 			}
 				break;
@@ -113,12 +120,25 @@ public class TargetManageOptionActivity extends Activity {
 
 	private void doAfterActionUp() {
 		if (startTarget != null && endTarget != null) {
-			Toast.makeText(this,
-					startTarget.getName() + "->" + endTarget.getName(),
-					Toast.LENGTH_SHORT).show();
-			startTarget = null;
-			endTarget = null;
+			if (startTarget.getName().equals(endTarget.getName())) {
+				long gap = times[1] - times[0];
+				if (gap > 1500) {
+					showWindowForAdd();
+				} else {
+					Toast.makeText(this,
+							"想要通过'" + endTarget.getName() + "'分身？请长按",
+							Toast.LENGTH_SHORT).show();
+				}
+			} else {
+				// Toast.makeText(this,
+				// startTarget.getName() + "->" + endTarget.getName(),
+				// Toast.LENGTH_SHORT).show();
+				showWindowForUpdate();
+			}
 		}
+		// 不管如何，都清零
+		startTarget = null;
+		endTarget = null;
 	}
 
 	private Target getBlockIndexByXY(float x, float y) {
@@ -195,7 +215,7 @@ public class TargetManageOptionActivity extends Activity {
 			Bitmap bm = BitmapFactory.decodeResource(getResources(),
 					R.drawable.s40).copy(Bitmap.Config.ARGB_8888, true);
 			cc.setBitmap(bm);
-			cc.drawCircle(20, 20, 20, p);
+			cc.drawCircle(20, 20, 10, p);
 
 			ImageView imageView = new ImageView(this);
 			imageView.setImageBitmap(bm);
@@ -207,11 +227,179 @@ public class TargetManageOptionActivity extends Activity {
 
 			l.addView(imageView);
 			l.addView(txt);
-			gridView.addView(l);
+			linearLayout.addView(l);
 		}
 	}
+
+	private void showWindowForAdd() {
+		if (startTarget == null) {
+			return;
+		}
+		initWindow();
+		popupAddLayout.setVisibility(View.VISIBLE);
+		popupUpdateLayout.setVisibility(View.INVISIBLE);
+		popupAddTitle.setText("adddddddddd");
+		popupAddTitle.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				hideWindow();
+			}
+		});
+		showWindow();
+	}
+
+	private void showWindowForUpdate() {
+		if (startTarget == null || endTarget == null) {
+			return;
+		}
+		initWindow();
+		popupAddLayout.setVisibility(View.INVISIBLE);
+		popupUpdateLayout.setVisibility(View.VISIBLE);
+		popupUpdateTitle.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				hideWindow();
+			}
+		});
+		showWindow();
+	}
+
+	private void initWindow() {
+		if (popupWindow == null) {
+			LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+			popupView = layoutInflater.inflate(
+					R.layout.main_target_manage_option_popup, null);
+			// 创建一个PopuWidow对象
+			popupUpdateLayout = (RelativeLayout) popupView
+					.findViewById(R.id.main_target_manage_option_popup_update_layout);
+			popupAddLayout = (RelativeLayout) popupView
+					.findViewById(R.id.main_target_manage_option_popup_add_layout);
+			popupWindow = new PopupWindow(popupView, 300, 350);
+			initLayout();
+			popupAddTitle.setText("添加分身");
+			popupUpdateTitle.setText("分身传输");
+		}
+	}
+
+	private void showWindow() {
+		// 使其聚集
+		popupWindow.setFocusable(true);
+		// 设置允许在外点击消失
+		popupWindow.setOutsideTouchable(true);
+		Point size = new Point();
+		getWindowManager().getDefaultDisplay().getSize(size);
+		int xPos = size.x / 2 - popupWindow.getWidth() / 2;
+		popupWindow.showAsDropDown(topBar, xPos, 20);
+	}
+
+	private void hideWindow() {
+		if (popupWindow != null) {
+			popupWindow.dismiss();
+		}
+	}
+
+	// window
+	private View popupView;
+	// update
+	private RelativeLayout popupUpdateLayout;
+	private TextView popupUpdateTitle;// main_target_manage_option_popup_update_title
+	private TextView popupUpdateStartTargetName;// main_target_manage_option_popup_update_start_target
+	private TextView popupUpdateEndTargetName;// main_target_manage_option_popup_update_end_target
+	private TextView popupUpdateSeekbarScaleLeft;// main_target_manage_option_popup_update_seekbar_left
+	private TextView popupUpdateSeekbarScaleRight;// main_target_manage_option_popup_update_seekbar_right
+	private SeekBar popupUpdateSeekbar;// main_target_manage_option_popup_update_seekbar
+	private Button popupUpdateButtonCancel;// main_target_manage_option_popup_update_button_cancel
+	private Button popupUpdateButtonConfirm;// addmain_target_manage_option_popup_update_button_confirm
+
+	private RelativeLayout popupAddLayout;
+	private TextView popupAddTitle;// main_target_manage_option_popup_add_title
+	private TextView popupAddStartTargetName;// main_target_manage_option_popup_add_start_target
+	private TextView popupAddEndTargetName;// main_target_manage_option_popup_add_end_target
+	private TextView popupAddSeekbarScaleLeft;// main_target_manage_option_popup_add_seekbar_left
+	private TextView popupAddSeekbarScaleRight;// main_target_manage_option_popup_add_seekbar_right
+	private SeekBar popupAddSeekbar;// main_target_manage_option_popup_add_seekbar
+	private Button popupAddButtonCancel;// main_target_manage_option_popup_add_button_cancel
+	private Button popupAddButtonConfirm;// addmain_target_manage_option_popup_add_button_confirm
+
+	private void initUpdateLayoutContent() {
+		popupUpdateStartTargetName.setText(startTarget.getName());
+		popupUpdateEndTargetName.setText(endTarget.getName());
+		popupUpdateSeekbarScaleLeft.setText(startTarget.getScale());
+		popupUpdateSeekbarScaleRight.setText(0);
+		popupUpdateSeekbar.setMax(startTarget.getScale()+endTarget.getScale());
+		popupUpdateSeekbar.setProgress(startTarget.getScale());
+		popupUpdateSeekbar
+				.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+					@Override
+					public void onStopTrackingTouch(SeekBar arg0) {
+
+					}
+
+					@Override
+					public void onStartTrackingTouch(SeekBar arg0) {
+					}
+
+					@Override
+					public void onProgressChanged(SeekBar arg0, int arg1,
+							boolean arg2) {
+
+					}
+				});
+		popupUpdateButtonCancel.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				hideWindow();
+			}
+		});
+		popupUpdateButtonConfirm.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				DemoDB<Target>db = new DemoDB<Target>(startTarget);
+				startTarget.setScale(popupUpdateSeekbar.getProgress());
+				endTarget.setScale(popupUpdateSeekbar.getMax()-popupUpdateSeekbar.getProgress());
+				db.update(obj, context)(new Target(name, scale), context)
+			}
+		});
+	}
+
+	private void initLayout() {
+		// update
+
+		popupUpdateTitle = (TextView) popupView
+				.findViewById(R.id.main_target_manage_option_popup_update_title);
+		popupUpdateStartTargetName = (TextView) popupView
+				.findViewById(R.id.main_target_manage_option_popup_update_start_target);// main_target_manage_option_popup_update_start_target
+		popupUpdateEndTargetName = (TextView) popupView
+				.findViewById(R.id.main_target_manage_option_popup_update_end_target);// main_target_manage_option_popup_update_end_target
+		popupUpdateSeekbarScaleLeft = (TextView) popupView
+				.findViewById(R.id.main_target_manage_option_popup_update_seekbar_left);// main_target_manage_option_popup_update_seekbar_left
+		popupUpdateSeekbarScaleRight = (TextView) popupView
+				.findViewById(R.id.main_target_manage_option_popup_update_seekbar_right);// main_target_manage_option_popup_update_seekbar_right
+		popupUpdateSeekbar = (SeekBar) popupView
+				.findViewById(R.id.main_target_manage_option_popup_update_seekbar);// main_target_manage_option_popup_update_seekbar
+		popupUpdateButtonCancel = (Button) popupView
+				.findViewById(R.id.main_target_manage_option_popup_update_button_cancel);// main_target_manage_option_popup_update_button_cancel
+		popupUpdateButtonConfirm = (Button) popupView
+				.findViewById(R.id.main_target_manage_option_popup_update_button_confirm);// addmain_target_manage_option_popup_update_button_confirm
+		// add
+		popupAddTitle = (TextView) popupView
+				.findViewById(R.id.main_target_manage_option_popup_add_title);
+		popupAddStartTargetName = (TextView) popupView
+				.findViewById(R.id.main_target_manage_option_popup_add_start_target);// main_target_manage_option_popup_add_start_target
+		popupAddEndTargetName = (TextView) popupView
+				.findViewById(R.id.main_target_manage_option_popup_add_end_target);// main_target_manage_option_popup_add_end_target
+		popupAddSeekbarScaleLeft = (TextView) popupView
+				.findViewById(R.id.main_target_manage_option_popup_add_seekbar_left);// main_target_manage_option_popup_add_seekbar_left
+		popupAddSeekbarScaleRight = (TextView) popupView
+				.findViewById(R.id.main_target_manage_option_popup_add_seekbar_right);// main_target_manage_option_popup_add_seekbar_right
+		popupAddSeekbar = (SeekBar) popupView
+				.findViewById(R.id.main_target_manage_option_popup_add_seekbar);// main_target_manage_option_popup_add_seekbar
+		popupAddButtonCancel = (Button) popupView
+				.findViewById(R.id.main_target_manage_option_popup_add_button_cancel);// main_target_manage_option_popup_add_button_cancel
+		popupAddButtonConfirm = (Button) popupView
+				.findViewById(R.id.main_target_manage_option_popup_add_button_confirm);// addmain_target_manage_option_popup_add_button_confirm
+	}
+	//
 }
-// c.setBitmap(currentBitmap);
-// c.drawArc(rectf, RandUtil.getInt(360), RandUtil.getInt(10),
-// true, p);
-// imageView.setImageBitmap(currentBitmap);
